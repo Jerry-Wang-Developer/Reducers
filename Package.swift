4c42566f8935c6d6052c8a74bf6937550a37fafb
Jerry Wang <445242970@qq.com>
2025-12-17 10:19:23 +0800

    Add support for watchOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Reducers",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
